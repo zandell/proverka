75bad175b0a601cdee96fb1ab326b2de34ca73cd
zandell <70910663+zandell@users.noreply.github.com>
2022-06-28 22:22:26 +0300

    first commit

---
+++ 1.bat
@@ -0,0 +1,7 @@
+git init
+git add .
+git config --global user.name zandell
+git commit -a -m "first commit"
+git remote add origin https://github.com/zandell/proverka.git
+git push -u origin main
+pause
